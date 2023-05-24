9a625e60ee3d11b6533b1cafbde2213057db06f0
Kaylin Chung <kay1inchung@g.ucla.edu>
2023-05-24 02:23:43 -0700

    Initial Commit

---
+++ project 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project 3
+//
+//  Created by Kaylin Chung on 5/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
